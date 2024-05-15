d265e006ac38258befff229b9615f54373cb48fe
Kashish Goyal <kashish1782004@gmail.com>
2024-05-16 00:28:43 +0530

    update data.py

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import time
 a=[1,2,3,4,5,6,7,8]
 n=np.array(a)
 nd=n.reshape((2,4))
@@ -12,6 +13,23 @@ dic={
 data=pd.read_csv("data//Salary_Data.csv")
 # print(data)
 
+# st.dataframe(a)
 # st.dataframe(n)
-st.dataframe()
-st.dataframe(nd)
+# st.dataframe(nd)
+st.dataframe(data, width=500,height=500)
+# or
+# st.table(data) # here we get all the data not a scroll bar
+st.json(dic)
+# st.write(data)
+
+# caching in streamlit
+@st.cache
+def ret_time(a):
+    time.sleep(5)
+    return time.time()
+
+if st.checkbox("1"):
+    st.write(ret_time(1))
+
+if st.checkbox("2"):
+    st.write(ret_time(2))
