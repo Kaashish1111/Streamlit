3ffceb7e181308fcc1aa5630a5203b436faf3a4f
Kashish Goyal <kashish1782004@gmail.com>
2024-05-15 23:56:26 +0530

    upadte data.py

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+a=[1,2,3,4,5,6]
+n=np.array(a)
+nd=n.reshape((2,4))
+dic={
+    "name":"Kashish"
+    "age":18
+    "Topic":Streamlit
+    
+}
