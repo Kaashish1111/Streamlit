b48d49069ae0419de81dcf97255e60b033007204
Kashish Goyal <kashish1782004@gmail.com>
2024-05-16 00:13:14 +0530

    data folder

--- data.py
+++ data.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-a=[1,2,3,4,5,6]
+a=[1,2,3,4,5,6,7,8]
 n=np.array(a)
 nd=n.reshape((2,4))
 dic={
-    "name":"Kashish"
-    "age":18
+    "name":"Kashish",
+    "age":18,
     "Topic":"Streamlit"
 }
-data=
+data=pd.read_csv("data//Salary_Data.csv")
+# print(data)
+
+# st.dataframe(n)
+st.dataframe()
+st.dataframe(nd)
