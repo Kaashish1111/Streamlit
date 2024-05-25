7a9ddfa72d5d85e4c6d29c22133b56117ae8ac57
Kashish Goyal <kashish1782004@gmail.com>
2024-05-25 13:15:59 +0530

    json updated

--- widget.py
+++ widget.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Widgets")
+if st.button("like"):
+    st.write("you like this")
+
+name=st.text_input("Name")
+description=st.text_area("About Me")
+st.write("Hello",name)
